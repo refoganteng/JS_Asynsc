55e7f077e8f28fba1460288ff98050fd7a5f8a64
refonando <user@MacBook-Pro-user.local>
2025-03-02 21:08:35 +0700

    tambah asynnc await

---
+++ app2.js
@@ -0,0 +1,14 @@
+// async function hello(){}
+
+const hello = async () => {
+	// throw 'Maaf gak kenal';
+	return 'Hello World';
+};
+
+hello()
+	.then((res) => {
+		console.log('response', res);
+	})
+	.catch((err) => {
+		console.log('error', err);
+	});
